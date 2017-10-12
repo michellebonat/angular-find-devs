b1b93c92e2fd434e9b7e38d94e747a5bb769c607
michellebonat <michelle.bonat@gmail.com>
2017-10-12 16:56:44 -0700

    Connect the service to the search method. Requires that user specify both search parameters, and throws a helpful error message if no results are found.

--- src/app/search-users/search-users.component.ts
+++ src/app/search-users/search-users.component.ts
@@ -10,17 +10,30 @@ export class SearchUsersComponent implements OnInit {
   place: string;
   language: string;
 
-  results: any[] = []; // This will hold the data coming from the service
+  results: any[] = []; // Holds the data coming from the service
   selected: boolean = false; // Flag to check if a user is clicked or not
-  selectedUser: any; // presently Selected user details
-  error_text: string = ""; // So called error reporing text to the end user
+  selectedUser: any; // Selected user details
+  error_text: string = ""; // Error reporing text to the end user
 
   constructor(private searchService: SearchUsersService) {}
   ngOnInit() {}
 
   search(place: string, language: string) {
-    this.place = place;
-    this.language = language;
-    console.log(this.place, this.language);
+    this.selected = false;
+    this.error_text = "";
+    if (place || language) {
+      this.place = place;
+      this.language = language;
+      this.searchService.getUsersByPlaceAndLanguage(place, language).subscribe(
+        users => {
+          this.results = users;
+        },
+        error => {
+          this.results = [];
+          this.error_text = "Drat! No Users found with those parameters. Please try again with a different combination.";
+          console.error(error);
+        }
+      )
+    }
   }
 }
